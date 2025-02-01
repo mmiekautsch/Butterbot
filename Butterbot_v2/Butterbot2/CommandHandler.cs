f6d7be429033cb4b3ce7b95f32e56d4a769a5df8
Marc-Oliver Miekautsch <marc-oliver.miekautsch@iu-study.org>
2025-02-01 16:18:31 +0100

    initial commit of Butterbot_v2

---
+++ Butterbot_v2/Butterbot2/CommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System.Reflection;
+
+namespace Butterbot2
+{
+    public class CommandHandler(DiscordSocketClient _client, CommandService _commandService)
+    {
+        private readonly DiscordSocketClient client = _client;
+        private readonly CommandService commandService = _commandService;
+
+        public string cmdPrefix = "!";
+
+        public async Task InstallCommandsAsync()
+        {
+            client.MessageReceived += HandleCommandAsync;
+            await commandService.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage message)
+        {
+            if (message is not SocketUserMessage msg) return;
+
+            int argPos = 0;
+
+            if (!msg.HasStringPrefix(cmdPrefix, ref argPos) ||
+                msg.HasMentionPrefix(client.CurrentUser, ref argPos) ||
+                msg.Author.IsBot) 
+                return;
+
+            SocketCommandContext context = new(client, msg);
+
+            await commandService.ExecuteAsync(context, argPos, null);
+        }
+    }
+}
---
+++ Butterbot_v2/Butterbot2/Program.cs
@@ -0,0 +1,30 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System.Diagnostics;
+
+#pragma warning disable CS8618 // Ein Non-Nullable-Feld muss beim Beenden des Konstruktors einen Wert ungleich NULL enthalten. Fügen Sie ggf. den „erforderlichen“ Modifizierer hinzu, oder deklarieren Sie den Modifizierer als NULL-Werte zulassend.
+
+namespace Butterbot2;
+
+public class Program
+{
+    private static DiscordSocketClient client;
+
+    public static async Task Main()
+    {
+        client = new();
+
+        client.Log += Log;
+
+        await client.LoginAsync(TokenType.Bot, File.ReadAllText("token.txt"));
+
+        await client.StartAsync();
+        await Task.Delay(-1);
+    }
+
+    private static Task Log(LogMessage msg)
+    {
+        Trace.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
